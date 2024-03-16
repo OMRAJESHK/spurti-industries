7e2e4bc9b6d4d498d617ddb1aac0c769c27f5a75
OMRAJESHK <omrajeshk6021@gmail.com>
2024-03-16 09:03:36 +0530

    working with navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +19,10 @@ export const metadata = {
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classes from "./navbar.module.css";
 
 const Navbar = (props) => {
-  return <div></div>;
+  return (
+    <div className={classes["nav-wrapper"]}>
+      <div className={classes["grad-bar"]}></div>
+      <nav className={classes["navbar"]}>
+        <img
+          className={classes["logo-image"]}
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bluestar_%28bus_company%29_logo.svg/1280px-Bluestar_%28bus_company%29_logo.svg.png"
+          alt="Company Logo"
+        />
+        <div className={classes["menu-toggle"]} id="mobile-menu">
+          <span className={classes["bar"]}></span>
+          <span className={classes["bar"]}></span>
+          <span className={classes["bar"]}></span>
+        </div>
+        <ul className={classes["nav"]}>
+          <li className={classes["nav-item"]}>
+            <a href="#">Home</a>
+          </li>
+          <li className={classes["nav-item"]}>
+            <a href="#">About</a>
+          </li>
+          <li className={classes["nav-item"]}>
+            <a href="#">Work</a>
+          </li>
+          <li className={classes["nav-item"]}>
+            <a href="#">Careers</a>
+          </li>
+          <li className={classes["nav-item"]}>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 Navbar.propTypes = {};
