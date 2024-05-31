b450b3b84c143f869502335b4e00b60103caea6b
OMRAJESHK <44435358+OMRAJESHK@users.noreply.github.com>
2024-06-01 01:32:43 +0530

    Create common.js inside navbar

---
+++ src/components/navbar/common.js
@@ -0,0 +1,8 @@
+import { pageRoutes } from "@/routes/routes";
+
+export const navbars = [
+  { id: 1, path: pageRoutes.home, label: "Home" },
+  { id: 2, path: pageRoutes.aboutus, label: "About Us" },
+  { id: 3, path: pageRoutes.products, label: "Products" },
+  { id: 4, path: pageRoutes.contactus, label: "Contact Us" },
+];
