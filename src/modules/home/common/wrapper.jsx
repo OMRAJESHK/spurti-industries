4ce698df422de08bbdda8305160975e6fa42a7f3
OMRAJESHK <44435358+OMRAJESHK@users.noreply.github.com>
2024-06-01 01:53:28 +0530

    Update wrapper.jsx

--- src/modules/home/common/wrapper.jsx
+++ src/modules/home/common/wrapper.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import classes from "../home.module.css";
 import Flexbox from "@/components/flexbox/flexbox";
 
-const Wrapper = ({ children }) => {
+const Wrapper = ({ children, classProp = "" }) => {
   return (
-    <Flexbox gap={20} classProp={classes["wrapper"]}>
+    <Flexbox
+      gap={20}
+      classProp={`${classProp ? classProp : ""} ${classes["wrapper"]}`}
+    >
       {children}
     </Flexbox>
   );
