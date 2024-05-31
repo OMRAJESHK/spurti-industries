e84cc95b0c01a5283571ce467751da8360aaa875
OMRAJESHK <44435358+OMRAJESHK@users.noreply.github.com>
2024-06-01 01:45:47 +0530

    Updated footer.jsx

--- src/modules/common/footer/footer.jsx
+++ src/modules/common/footer/footer.jsx
@@ -1,6 +1,11 @@
 import Flexbox from "@/components/flexbox/flexbox";
 import React from "react";
 import classes from "./footer.module.css";
+import CustomImage from "@/components/customImage";
+import facebookIcon from "../../../asset/svgs/facebook.svg";
+import linkedinIcon from "../../../asset/svgs/linkedIn.svg";
+import twitterIcon from "../../../asset/svgs/twitter.svg";
+import mailIcon from "../../../asset/svgs/mail.svg";
 
 const Footer = () => {
   return (
@@ -9,26 +14,25 @@ const Footer = () => {
         <h2 className={classes["footer-company-title"]}>Spurthi Industries</h2>
         <div className={classes["footer-address-wrapper"]}>
           <p>
-            KARNATAKA AUTOMATS PVT. LTD. #143/F, AKS Building, Sy. No.
-            70.180.181, Bommasandra Industrial Area, Hebbagodi Village,
-            Bangalore KA 560099
+            No 65/4 4th Cross KG Shivanna Building Kaveripura, Kamakshipalya
+            Bangalore Bengaluru - 560079, Karnataka, India
           </p>
         </div>
       </div>
       <div className={classes["footer-item-wrapper"]}>
         <h2 className={classes["footer-title"]}>Quick Contacts</h2>
         <div className={classes["footer-contacts-wrapper"]}>
-          <p>Mail : support@spurthiindustries.com</p>
-          <p>Mobile : +91 9865230124</p>
-          <p>Fax : +91 (80) 45145500</p>
+          <p>Mail : spurthi_industries2003@yahoo.co.in</p>
+          <p>Mobile : +91 9844130388</p>
         </div>
       </div>
       <div className={classes["footer-item-wrapper"]}>
         <h2 className={classes["footer-title"]}>Social Media Links</h2>
         <div className={classes["footer-contacts-wrapper"]}>
-          <p>Mail : support@spurthiindustries.com</p>
-          <p>Mobile : +91 9865230124</p>
-          <p>Fax : +91 (80) 45145500</p>
+          <CustomImage src={facebookIcon} classProp={classes["card-img"]} />
+          <CustomImage src={linkedinIcon} classProp={classes["card-img"]} />
+          <CustomImage src={twitterIcon} classProp={classes["card-img"]} />
+          <CustomImage src={mailIcon} classProp={classes["card-img"]} />
         </div>
       </div>
     </Flexbox>
