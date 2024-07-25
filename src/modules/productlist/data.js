05db80020af25058a00dc0436deba5c82e2b0526
OMRAJESHK <omrajeshk6021@gmail.com>
2024-07-25 18:39:39 +0530

    SI | Rajesh | relevent products added to product list

--- src/modules/productlist/data.js
+++ src/modules/productlist/data.js
@@ -1,54 +1,47 @@
-import img1 from "../../asset/images/image-1.jpeg";
-import img3 from "../../asset/images/image-3.jpeg";
-import img4 from "../../asset/images/image-4.jpeg";
-import img5 from "../../asset/images/image-5.jpeg";
-import img6 from "../../asset/images/image-6.jpeg";
-import img7 from "../../asset/images/image-7.jpeg";
-import img8 from "../../asset/images/image-8.jpeg";
-import img9 from "../../asset/images/image-9.jpeg";
-import img10 from "../../asset/images/image-10.jpeg";
-import img11 from "../../asset/images/image-11.jpeg";
-import img12 from "../../asset/images/image-12.jpeg";
-import img13 from "../../asset/images/image-13.jpeg";
-import img14 from "../../asset/images/image-14.jpeg";
-import img15 from "../../asset/images/image-15.jpeg";
-import img16 from "../../asset/images/image-16.jpeg";
-import img17 from "../../asset/images/image-17.jpeg";
-import img18 from "../../asset/images/image-18.jpeg";
-import img19 from "../../asset/images/image-19.jpeg";
-import img20 from "../../asset/images/image-20.jpeg";
-import img21 from "../../asset/images/image-21.jpeg";
-import img22 from "../../asset/images/image-22.jpeg";
-import img23 from "../../asset/images/image-23.jpeg";
-import img24 from "../../asset/images/image-24.jpeg";
-import img25 from "../../asset/images/image-25.jpeg";
-import img26 from "../../asset/images/image-2.jpeg";
+import product1 from "../../asset/images/image-2.jpg";
+import product2 from "../../asset/images/image-5.jpg";
+import product3 from "../../asset/images/image-6.jpeg";
+import product4 from "../../asset/images/image-7.jpg";
+import product5 from "../../asset/images/image-10.jpeg";
+import product6 from "../../asset/images/image-10.jpg";
+import product7 from "../../asset/images/image-12.jpeg";
+import product8 from "../../asset/images/image-13.jpeg";
+import product9 from "../../asset/images/image-14.jpeg";
+import product10 from "../../asset/images/image-15.jpeg";
+import product11 from "../../asset/images/image-16.jpeg";
+import product12 from "../../asset/images/image-13.jpeg";
+import product13 from "../../asset/images/image-14.jpeg";
+import product14 from "../../asset/images/image-15.jpeg";
+import product15 from "../../asset/images/image-16.jpeg";
+import product16 from "../../asset/images/image-17.jpeg";
+import product17 from "../../asset/images/image-18.jpeg";
+import product18 from "../../asset/images/image-19.jpeg";
+import product19 from "../../asset/images/image-20.jpeg";
+import product20 from "../../asset/images/image-21.jpeg";
+import product21 from "../../asset/images/image-22.jpeg";
+import product22 from "../../asset/images/image-23.jpeg";
 
 export const imageGallery = [
-  { id: 1, src: img1 },
-  { id: 2, src: img3 },
-  { id: 3, src: img4 },
-  { id: 4, src: img5 },
-  { id: 5, src: img6 },
-  { id: 6, src: img7 },
-  { id: 7, src: img8 },
-  { id: 8, src: img9 },
-  { id: 9, src: img10 },
-  { id: 10, src: img11 },
-  { id: 11, src: img12 },
-  { id: 12, src: img13 },
-  { id: 13, src: img14 },
-  { id: 14, src: img15 },
-  { id: 15, src: img16 },
-  { id: 16, src: img17 },
-  { id: 17, src: img18 },
-  { id: 18, src: img19 },
-  { id: 19, src: img20 },
-  { id: 20, src: img21 },
-  { id: 21, src: img21 },
-  { id: 22, src: img22 },
-  { id: 23, src: img23 },
-  { id: 24, src: img24 },
-  { id: 25, src: img25 },
-  { id: 26, src: img26 },
+  { id: 1, src: product1 },
+  { id: 2, src: product2 },
+  { id: 3, src: product3 },
+  { id: 4, src: product4 },
+  { id: 5, src: product5 },
+  { id: 6, src: product6 },
+  { id: 7, src: product7 },
+  { id: 8, src: product8 },
+  { id: 9, src: product9 },
+  { id: 10, src: product10 },
+  { id: 11, src: product11 },
+  { id: 12, src: product12 },
+  { id: 13, src: product13 },
+  { id: 14, src: product14 },
+  { id: 15, src: product15 },
+  { id: 16, src: product16 },
+  { id: 17, src: product17 },
+  { id: 18, src: product18 },
+  { id: 19, src: product19 },
+  { id: 20, src: product20 },
+  { id: 21, src: product21 },
+  { id: 22, src: product22 },
 ];
